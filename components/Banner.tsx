86d37e11c85b86eeac0d576a58b0a32d7e2eb744
trungsine <trungfb20@gmail.com>
2025-01-25 21:00:32 +0700

    responsive topbar,banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import "../styles/banner.css";
 
 function Banner() {
   return (
-    <div className="banner-top-container flex h-10 items-center justify-evenly">
-      <div>
+    <div className="bg-[#e9efff] flex h-10 items-center justify-evenly">
+      <div className="block sm:hidden">
         <a href="#">
           <img
             className="h-[30px]"
@@ -13,7 +12,16 @@ function Banner() {
           />
         </a>
       </div>
-      <div>
+      <div className="hidden sm:block">
+        <a href="#">
+          <img
+            className="h-[30px]"
+            src="https://cdn2.cellphones.com.vn/x/https://dashboard.cellphones.com.vn/storage/Top banner_Giao hang.svg"
+            alt=""
+          />
+        </a>
+      </div>
+      <div className="hidden sm:block">
         <a href="#">
           <img
             className="h-[30px]"
@@ -22,7 +30,7 @@ function Banner() {
           />
         </a>
       </div>
-      <div>
+      <div className="hidden md:block">
         <a href="#">
           <img
             className="h-[30px]"
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-// import css
-import "../styles/header.css";
-
 import { IoMdListBox } from "react-icons/io";
 import { CiLocationOn } from "react-icons/ci";
 import { MdKeyboardArrowDown } from "react-icons/md";
@@ -13,38 +10,51 @@ import { FaRegUserCircle } from "react-icons/fa";
 
 function Header() {
   return (
-    <div className="header-bgcolor">
-      <div className="header-container">
-        <div className="header-logo">
+    <div className="justify-center bg-[#e1042a] w-full flex">
+      <div className="header-container w-[1200px] bg-[#e1042a] flex items-center justify-between text-xs text-[#fff] relative h-[64px]">
+        <div>
           <img
+            className="w-[170px] h-[40px] hidden sm:block"
             src="https://cdn2.cellphones.com.vn/x/media/wysiwyg/logo_cps-1.png"
             alt="logo-cellphones"
           />
         </div>
 
-        <div className="header-listBox">
-          <IoMdListBox />
-          <h2>Danh mục</h2>
+        <div>
+          <img
+            className="sm:block w-8 h-8 border border-solid border-white rounded-[10px]"
+            src="https://yt3.googleusercontent.com/rK8cTvkOXk2t-f1xlBTsyx4VtHjrKWdBPKHaMTUdxTuQBY3oZ8Gok-H2NZPwp3aJAYO_cRLO=s900-c-k-c0x00ffffff-no-rj"
+            alt="logo-cellphones"
+          />
         </div>
 
-        <div className="header-locationPrice">
-          <CiLocationOn />
+        <div className="flex items-center hidden sm:block rounded-[10px] cursor-pointer bg-[#df3346] px-[10px] py-[9px]">
+          <IoMdListBox className="text-2xl mr-[5px]" />
+          <h2 className="text-xs m-0">Danh mục</h2>
+        </div>
+
+        <div className="flex items-center hidden sm:block rounded-[10px] cursor-pointer bg-[#df3346] px-[10px] py-[3px]">
+          <CiLocationOn className="text-2xl mr-[5px]" />
           <div>
-            <div>
-              <h2>Xem giá tại</h2>
+            <div className="flex">
+              <p className="text-[10px] font-semibold m-0">Xem giá tại</p>
               <MdKeyboardArrowDown />
             </div>
-            <h2>Hồ Chí Minh</h2>
+            <span className="text-sm font-light">Hồ Chí Minh</span>
           </div>
         </div>
 
-        <div className="header-search">
-          <FaSearch />
-          <input type="text" placeholder="Bạn cần tìm gì?" />
+        <div className="flex items-center rounded-[10px] bg-white pl-[10px]">
+          <FaSearch className="text-gray-500 text-[15px]" />
+          <input
+            className="outline-none py-[8px] pr-[32px] pl-[6px] rounded-[10px] text-black"
+            type="text"
+            placeholder="Bạn cần tìm gì?"
+          />
         </div>
 
-        <div className="header-phone">
-          <FiPhone />
+        <div className="cursor-pointer hidden sm:block h-[42px] flex items-center rounded-[10px] hover:bg-[#df3346] py-[3px] px-[10px]">
+          <FiPhone className="text-2xl mr-[5px]" />
           <div>
             <span>
               Gọi mua hàng <br /> 1800.2097
@@ -52,34 +62,32 @@ function Header() {
           </div>
         </div>
 
-        <div className="header-locationStore">
-          <CiLocationOn />
+        <div className="cursor-pointer hidden sm:block h-[42px] flex items-center rounded-[10px] hover:bg-[#df3346] py-[3px] px-[10px]">
+          <CiLocationOn className="text-2xl mr-[5px]" />
 
-          <h2>
+          <h2 className="text-xs m-0">
             Cửa hàng <br /> gần bạn
           </h2>
         </div>
 
-        <div className="header-truckDelivery">
-          <LiaShippingFastSolid />
-          <h2>
+        <div className="cursor-pointer hidden sm:block h-[42px] flex items-center rounded-[10px] hover:bg-[#df3346] py-[3px] px-[10px]">
+          <LiaShippingFastSolid className="text-2xl mr-[5px]" />
+          <h2 className="text-xs m-0">
             Tra cứu <br /> đơn hàng
           </h2>
         </div>
 
-        <div className="header-shoppingBag">
-          <IoBagOutline />
+        <div className="cursor-pointer h-[42px] flex items-center rounded-[10px] hover:bg-[#df3346] py-[3px] px-[10px]">
+          <IoBagOutline className="text-2xl mr-[5px]" />
 
-          <h2>
+          <h2 className="text-xs m-0">
             Giỏ <br /> hàng
           </h2>
-
-          {/* <h2>Giỏ hàng</h2> */}
         </div>
 
-        <div className="header-userLogin">
-          <FaRegUserCircle />
-          <h2>Đăng nhập</h2>
+        <div className="flex hidden sm:block flex-col items-center text-center py-[3px] px-[10px] rounded-[10px] bg-[#df3346]">
+          <FaRegUserCircle className="text-base mr-[5px]" />
+          <h2 className="text-xs m-0">Đăng nhập</h2>
         </div>
       </div>
     </div>
