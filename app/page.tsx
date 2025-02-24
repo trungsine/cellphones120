5be9b5cd33250a8d7aa14988208a0ac254d49f5d
trungsine <trungfb20@gmail.com>
2025-02-24 22:02:57 +0700

    fix scroll 80%

--- app/page.tsx
+++ app/page.tsx
@@ -14,10 +14,12 @@ import useStore from "../store/store";
 export default function Home() {
   const [showDropdown, setShowDropdown] = useState(false);
   const isActive = useStore((state) => state.isActive);
-
+  const [windowWidth] = useState(window.innerWidth);
   const handleToggleDropdown = () => {
     setShowDropdown((prev) => !prev);
   };
+
+  const innerDisplayStyle = windowWidth > 768 ? "none" : "block";
 
   return (
     <main>
@@ -29,6 +31,7 @@ export default function Home() {
 
         <div style={{ display: isActive ? "none" : "block" }}>
           <SlideProduct id="popup-portal" />
+
           <BannerBottom />
         </div>
         <MenuBottom />
--- components/MenuBottom.tsx
+++ components/MenuBottom.tsx
@@ -17,6 +17,8 @@ const MenuBottom = () => {
   const [hoverIndex, setHoverIndex] = useState<number | null>(0);
   // const [isActive, setIsActive] = useState<boolean>(false);
 
+  const indexValue: number[] = [];
+
   const colors = [
     "rgb(253,180,180)",
     "rgb(255,237,213)",
@@ -41,111 +43,121 @@ const MenuBottom = () => {
     setIsActive(false);
   };
 
+  console.log(hoverIndex);
+
   return (
     <>
       {/* Popup Active */}
       {isActive ? (
         <>
-          <div className="relative h-[400px] overflow-scroll">
-            {listItemMobile.map((currentValue, index) => {
-              const backgroundColor = colors[index % colors.length];
-              return (
-                <>
-                  <div
-                    key={index}
-                    style={{ backgroundColor }}
-                    className="relative w-20  px-[5px] h-20 flex flex-col justify-center"
-                    onMouseEnter={() => setHoverIndex(index)}
-                  >
-                    <img
-                      src={currentValue.logo}
-                      alt="logo"
-                      width={40}
-                      height={40}
-                      className="mx-auto"
-                    />
-                    <a className=" text-xs text-[#444]  font-bold  text-center">
-                      {currentValue.title}
-                    </a>
-                  </div>
-                  <div className="">
-                    {hoverIndex === index && (
-                      <div className="absolute overflow-y-scroll z-10 top-0 left-20 max-w-[684px] p-[10px] border border-solid border-red-400">
-                        {/* ===  Box Title === */}
+          <div className="block md:hidden relative">
+            {/* Left */}
+            <div className="relative h-[400px] overflow-y-auto scrollbar-hide">
+              {listItemMobile.map((currentValue, index) => {
+                const backgroundColor = colors[index % colors.length];
+                indexValue.push(index);
+                return (
+                  <>
+                    <div
+                      key={index}
+                      style={{ backgroundColor }}
+                      className="relative w-20  px-[5px] h-20 flex flex-col justify-center overflow-hidden"
+                      onMouseEnter={() => setHoverIndex(index)}
+                    >
+                      <img
+                        src={currentValue.logo}
+                        alt="logo"
+                        width={40}
+                        height={40}
+                        className="mx-auto"
+                      />
+                      <a className=" text-xs text-[#444]  font-bold  text-center">
+                        {currentValue.title}
+                      </a>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+
+            {/*   Right */}
+            <div className="p-[10px] absolute top-0 left-20 max-w-[687px] h-[400px]  overflow-y-auto scrollbar-hide ">
+              {listItemMobile.map((a, b) => {
+                return (
+                  <div key={b}>
+                    {hoverIndex === b && (
+                      <>
                         <div className="flex justify-between">
-                          <a
-                            href="#"
-                            className="text-base text-[#444] font-bold leading-6"
-                          >
-                            {currentValue.titleLeft}
+                          <a href="" className=" text-[#444] font-bold">
+                            {a.titleLeft}
                           </a>
-                          <a href="#" className="text-sm text-[#444]">
-                            {currentValue.titleRight}
+                          <a href="" className="text-sm text-[#444]">
+                            {a.titleRight}
                           </a>
                         </div>
-                        {/* === End Product Detail === */}
-
-                        {/* Product Detail */}
-                        {currentValue.contentTitle.map((item, index) => (
-                          <div key={index} className="overflow-y-auto">
-                            <div>
-                              <h3 className="text-base text-[#363636] font-bold">
-                                {item.titleItem}
-                              </h3>
-                            </div>
-                            <div className="flex flex-wrap w-full">
-                              {item.contentTitleItem.map(
-                                (valueChild, childIndex) => {
-                                  // kiểm tra url
-                                  // const isUrl = valueChild.startsWith("https");
-                                  return (
-                                    <div
-                                      key={childIndex}
-                                      className={`${
-                                        valueChild.contentTitleDes &&
-                                        valueChild.contentTitleImg
-                                          ? "flex-col"
-                                          : ""
-                                      }  flex items-center my-[5px] mr-[10px] px-[10px] py-[5px] border border-solid border-[#d1d5db] rounded-[10px] min-h-[34px]`}
-                                    >
-                                      {valueChild.contentTitleImg ? (
-                                        <div className="w-[50px] ">
-                                          <a className="">
-                                            <img
-                                              src={valueChild.contentTitleImg}
-                                              alt=""
-                                              className=""
-                                            />
-                                          </a>
+                        {a.contentTitle.map((c, d) => {
+                          return (
+                            <>
+                              <div key={d} className="overflow-y-auto">
+                                <div className="overflow-hidden">
+                                  <h3 className="text-base text-[#363636] font-bold">
+                                    {c.titleItem}
+                                  </h3>
+                                </div>
+                                <div className="flex flex-wrap  custom-width-mobile">
+                                  {c.contentTitleItem.map(
+                                    (valueChild, index) => {
+                                      // const isUrl = valueChild.startsWith("https")
+                                      return (
+                                        <div
+                                          key={index}
+                                          className={`${
+                                            valueChild.contentTitleDes &&
+                                            valueChild.contentTitleImg
+                                              ? "flex-col"
+                                              : ""
+                                          }  flex items-center my-[5px] mr-[10px] px-[10px] py-[5px] border border-solid border-[#d1d5db] rounded-[10px] min-h-[34px]`}
+                                        >
+                                          {valueChild.contentTitleImg ? (
+                                            <div className="w-[50px] ">
+                                              <a className="">
+                                                <img
+                                                  src={
+                                                    valueChild.contentTitleImg
+                                                  }
+                                                  alt=""
+                                                  className=""
+                                                />
+                                              </a>
+                                            </div>
+                                          ) : null}
+                                          {valueChild.contentTitleDes ? (
+                                            <div>
+                                              <a className="text-xs text-[#4a4a4a] py-[5px] px-[10px] w-[50px] w-52">
+                                                {valueChild.contentTitleDes}
+                                              </a>
+                                            </div>
+                                          ) : null}
                                         </div>
-                                      ) : null}
-                                      {valueChild.contentTitleDes ? (
-                                        <div>
-                                          <a className="text-xs text-[#4a4a4a] py-[5px] px-[10px] w-[50px]">
-                                            {valueChild.contentTitleDes}
-                                          </a>
-                                        </div>
-                                      ) : null}
-                                    </div>
-                                  );
-                                }
-                              )}
-                            </div>
-                          </div>
-                        ))}
-                        {/* === End Product Detail === */}
-                      </div>
+                                      );
+                                    }
+                                  )}
+                                </div>
+                              </div>
+                            </>
+                          );
+                        })}
+                      </>
                     )}
                   </div>
-                </>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
         </>
       ) : null}
-
       {/* Bottom */}
-      <div className="block md:hidden sticky top-0 h-[72px] py-[10px] border border-solid  rounded-t-2xl">
+      <div className="block md:hidden z-50 sticky top-0 h-[72px] py-[10px] border border-solid  rounded-t-2xl">
         <ul className="flex justify-around h-full items-center mb-0">
           <li className="p-[5px]">
             <a
